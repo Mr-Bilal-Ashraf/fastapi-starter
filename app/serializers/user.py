861492011195eff07d5b84c588fca0e69b29683c
Muhammad Bilal Azaad <bilal@fabtechsol.com>
2024-12-02 13:06:53 +0500

    feat: Sign Up & response serializers created

---
+++ app/serializers/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+
+from datetime import datetime
+
+
+class UserResponse(BaseModel):
+    id: int
+    # username: str
+    email: str
+    first_name: str
+    last_name: str
+    date_of_birth: str = None
+    profile_picture: str = None
+    last_login: datetime = None
+    is_active: bool
+    is_superuser: bool
+    date_joined: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    # username: str = Field(min_length=1, max_length=50)
+    email: str = Field(min_length=15, max_length=70)
+    password: str = Field(min_length=8, max_length=50)
+    first_name: str = Field(min_length=5, max_length=150)
+    last_name: str | None = Field(min_length=5, max_length=150)
