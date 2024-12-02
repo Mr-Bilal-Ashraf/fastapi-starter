150f5a7b2937f252eea0224de12d1180bdb5042a
Muhammad Bilal Azaad <bilal@fabtechsol.com>
2024-12-02 13:07:24 +0500

    feat: Sign Up API created

---
+++ app/api/v1/user.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from app.serializers.user import UserCreate
+from app.dependencies import get_session
+from app.models.user import User
+
+router = APIRouter()
+
+
+@router.post("v1/users/create/")
+async def create_new_user(
+    user: UserCreate, bg_tasks: BackgroundTasks, db: Session = Depends(get_session)
+):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already in use.")
+
+    obj = User(**user.model_dump())
+    obj.set_password(user.password)
+    db.add(obj)
+    db.commit()
+    resp = {
+        "id": obj.id,
+        "email": obj.email,
+        "msg": f"An Activation token is sent to {obj.email}.",
+    }
+    return JSONResponse(content=resp, status_code=status.HTTP_201_CREATED)
