9bd32cc8f3a0f65ad4a010f2beabb1efecc37c97
Muhammad Bilal Azaad <bilal@fabtechsol.com>
2024-12-02 13:08:54 +0500

    feat: Bind static files and API routes & import new models, so that they can be created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, staticfiles
 
 from app.dependencies import engine
 from app.models.base import Base
@@ -37,11 +37,19 @@ app.add_middleware(GZipMiddleware)  # Compress responses
 
 
 # Include API routes
-# app.include_router(endpoints.router, prefix="/api/v1")
+def configure_routing():
+    from app.api.v1 import user
+
+    app.mount("/static", staticfiles.StaticFiles(directory="app/static"), name="static")
+    # app.include_router(base.router, prefix="/base", tags=["base"])
+    app.include_router(user.router, prefix="/user", tags=["user"])
 
 @app.on_event("startup")
 def startup_event():
+    from app.models.base import OTP
+    from app.models.user import User
     Base.metadata.create_all(engine)
+    configure_routing()
 
 
 @app.get("/ping")
