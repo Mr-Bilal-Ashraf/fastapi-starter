619369977e1d018e1e7b60ae4295d82c6dae90ca
Muhammad Bilal Azaad <bilal@fabtechsol.com>
2024-12-03 13:56:12 +0500

    feat: account activation email utility

--- app/utils.py
+++ app/utils.py
@@ -52,3 +52,11 @@ def send_email(to_email: str, subject: str, body: str):
         print(f"Error sending email: {e}")
         return False
     return True
+
+
+def account_activation_email(db: Session, user: User):
+    otp = create_otp(db, user.id, OTPChoices.ACCOUNT_ACTIVATION)
+    subject = "FastAPI Account Activation Token"
+    name = f"{user.first_name}{' ' + user.last_name if user.last_name else ''}"
+    body = f"Hi {name}, Your OTP for account activation is: {otp}"
+    return send_email(user.email, subject, body)
